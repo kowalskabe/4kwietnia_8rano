e9aa3be44abe1f89552155c5e8f3ac0f9dee63d3
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-04-04 10:04:41 +0200

    Dodano pola i właściwości klasy Garaz

--- Garaz.cs
+++ Garaz.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Laboratory1
 {
-	public Class1()
-	{
-	}
+    class Garaz
+    {
+        private string adres;
+        private int pojemnosc;
+        private int liczbaSamochodow = 0;
+        private Samochod[] samochody;
+
+   
+        public string Adres
+        {
+            get { return adres; }
+            set { adres = value; }
+        }
+
+        public int Pojemnosc
+        {
+            get { return pojemnosc; }
+            set 
+            { 
+                pojemnosc = value;
+                samochody = new Samochod[pojemnosc];
+            }
+        }
+    }
 }
