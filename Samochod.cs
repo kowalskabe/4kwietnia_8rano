4f0d7a5d7da7a88b257cc623e481a0f6fd7b0cab
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-04-04 08:55:59 +0200

    Dopisałam testy, pola i właściwości dostępowe.

--- Samochod.cs
+++ Samochod.cs
@@ -9,5 +9,40 @@ namespace Laboratory1
     class Samochod
     {
         private string marka;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        private double srednieSpalanie;
+
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka  = value; }
+        }
+
+        public string Model { get; set; }
+
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+
+        public int PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+
+        public double SrednieSpalanie
+        {
+            get { return srednieSpalanie; }
+            set { srednieSpalanie = value; }
+        }
+
+        private static int iloscSamochodow = 0;
+
+
     }
 }
